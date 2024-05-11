2f3a4364ab003c9404e5da6f99c15ed0e7ed13e5
IsaiahSama <zeldaplayergl@gmail.com>
2024-05-11 15:45:09 -0400

    Added additional Enums

--- scripts/utils/enums.py
+++ scripts/utils/enums.py
@@ -3,21 +3,44 @@ from enum import Enum
 
 
 class RestaurantNames(Enum):
-    """Enum mapping restaurant keys, to their names and abbreviations.
-    
+    """
+    An enum for the names of the 4AM restaurants, along with their corresponding abbreviations.
+
     Example: TIDES = ('Tides', 'TPS')
-    
-    Attributes:
-        TIDES
-
-        BISTRO
-
-        CLIFF
-        
-        CAFE
     """
 
     TIDES = ("Tides", "TPS")
     BISTRO = ("QP Bistro", "QPB")
     CLIFF = ("The Cliff", "QPC")
     CAFE = ("Cafe de Paris", "CDP")
+
+class SalesColumnNames(Enum):
+    """
+    An enum for the column names used in the Sales spreadsheet.
+
+    This enum is used to ensure that the correct column names are used when generating reports.
+    """
+
+    ID = "Order ID"
+    DATE = "Date"
+    SESSION = "Session"
+    CATEGORY = "Category"
+    GUESTS = "Guests"
+    AMOUNT = "Sale Net"
+    LEVY = "Levy"
+    VAT = "Vat"
+
+class PaymentColumnNames(Enum):
+    """
+    An enum for the column names used in the Payment spreadsheet.
+
+    This enum is used to ensure that the correct column names are used when generating reports.
+    """
+    
+    ID = "Order ID"
+    STORE = "Store"
+    PAYMENT_TYPE = "Type"
+    AMOUNT = "Amt"
+    CURRENCY = "Curr."
+    EXCHANGE = "Exch"
+    GROSS = "Order Gross"
