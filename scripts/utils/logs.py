9fa09d1228003fc7437290fdd89e570b880ef9fc
IsaiahSama <zeldaplayergl@gmail.com>
2024-06-10 09:11:27 -0700

    Began setup for Logging

---
+++ scripts/utils/logs.py
@@ -0,0 +1,5 @@
+"""This file will be responsible for handling the logging of the script."""
+import logging
+from .enums import RestaurantNames
+from datetime import datetime
+from .custom_exceptions import InvalidRestaurantNameException
