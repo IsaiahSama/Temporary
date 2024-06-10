71dbefe9216d2412f83ad699dabd19451f04d814
IsaiahSama <zeldaplayergl@gmail.com>
2024-06-10 09:14:28 -0700

    Added controller script for generating comp summaries

--- comp_gen.py
+++ comp_gen.py
@@ -1,1 +1,13 @@
 """This code will be responsible for creating a comp summary report."""
+
+from datetime import datetime, timedelta
+from scripts.generate_comp import CompSummaryGenerator
+
+last_week = datetime.now() - timedelta(days = 2)
+
+generator = CompSummaryGenerator()
+
+restaurants = ["CAFE", "CLIFF", "TIDES", "BISTRO"]
+
+for restaurant in restaurants:
+    generator.generate_comp_summary(restaurant, last_week)
